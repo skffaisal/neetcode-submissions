234ca7b5019821736b713336b61a119eb1681fa2
SHAIKH FAISAL <89987477+skffaisal@users.noreply.github.com>
2026-09-25 16:39:08 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, n: List[int]) -> int:
+
+        min_val=n[0] # coz of negative inputs
+        best_diff= 0 # becasue we want profit 
+
+        for i in range(len(n)):
+
+            min_val=min(min_val,n[i])
+        
+            diff = n[i]-min_val
+
+            if best_diff< diff:
+                best_diff =diff
+
+        return best_diff
+        
